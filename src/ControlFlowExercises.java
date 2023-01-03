b9bebd7237b038b3e0067a43ff7200205aa56e2a
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2023-01-03 14:59:05 -0700

    adding control flow exercises progress

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,65 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+
+    /////////////////// #1 a. ////////////////////////
+//        int i = 5;
+//        while (i <= 15){
+//            System.out.print(i + " ");
+//            i++;
+//        }
+
+    /////////////////// #1 b. ////////////////////////
+//        /////b1/////
+//        int i = 0;
+//        do {
+//            if (i % 2 == 0){
+//                System.out.println(i);
+//            }
+//            i++;
+//        }while (i <= 100);
+//
+//        /////b2/////
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        }while (i != -15);
+//
+//        /////b3/////
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i *= i;
+//        }while (i < 1000000);
+
+
+    /////////////////// #1 c. ////////////////////////
+
+        //a refactored with for loop
+//        for (int i = 5; i <= 15; i++){
+//            System.out.print(i + " ");
+//        }
+//
+//        //b1 refactored with for loop
+//        for (int i = 0; i <= 100; i++){
+//            if (i % 2 == 0){
+//                System.out.println(i);
+//            }
+//        }
+//        //b2 refactored with for loop
+//        for (int i = 100; i != -15; i -= 5){
+//            System.out.println(i);
+//        }
+//        //b3 refactored with for loop
+//        for (long i = 2; i < 1000000; i*=i){
+//            System.out.println(i);
+//        }
+
+
+    /////////////////// #2 ////////////////////////
+
+
+
+
+    } //main close
+} //class close
