bd09bc70cc51e4d60730a9d8f7173799d8820133
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2023-01-13 10:13:35 -0700

    adding collections grades app exercise

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,10 +1,13 @@
 package grades;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class GradesApplication {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
 
         HashMap<String, Student> students = new HashMap<>();
 
@@ -32,6 +35,32 @@ public class GradesApplication {
         kenny.addGrade(90);
         students.put("muffledSounds", kenny);
 
+        System.out.println("\n\nWelcome!\n");
+        System.out.println("Here are the Github usernames of our students: \n");
+
+        for (HashMap.Entry<String, Student> entry : students.entrySet()) {
+            System.out.print(entry.getKey() + " | ");
+        }
+        System.out.println("");
+
+        boolean keepGoing = true;
+        do {
+            System.out.println("\nWhat student would you like to see more information on?");
+            String studentPick = scanner.nextLine();
+
+            if (students.containsKey(studentPick)){
+                System.out.println("\nName: " + students.get(studentPick).getName() + " - Github Username: " + studentPick);
+                System.out.printf("Current Average: %.2f%n", students.get(studentPick).getGradeAverage());
+            } else {
+                System.out.println("\nSorry, not student found with the Github username of \"" + studentPick + "\".");
+            }
+            System.out.println("\nWould you like to see another student?");
+            String userY = scanner.nextLine();
+            if (userY.equalsIgnoreCase("n") || userY.equalsIgnoreCase("no")){
+                System.out.println("\n\nGoodbye, and have a wonderful day!");
+                keepGoing =  false;
+            }
+        } while (keepGoing);
 
 
     }
