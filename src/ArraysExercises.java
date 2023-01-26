a481fbb8539e62ba8e893ba31fd348b4533aa9d5
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2023-01-26 15:05:58 -0700

    fixing typo

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,7 +4,7 @@ public class ArraysExercises {
 
     public static Person[] addPerson(Person[] array, Person newPerson) {
         Person[] people = Arrays.copyOf(array, array.length + 1);
-        people[array.length] = newPerson; // Also works: people[people.length - 1]
+        people[array.length] = newPerson;
         return people;
     }
 
