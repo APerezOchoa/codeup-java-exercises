0573c449027baa9a1b45bfeb872c35376fcc9ea3
Antonio Perez Ochoa <antonio.perezochoa1@gmail.com>
2023-01-26 15:05:19 -0700

    adding arrays exercises properly

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,13 +1,21 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
+
+    public static Person[] addPerson(Person[] array, Person newPerson) {
+        Person[] people = Arrays.copyOf(array, array.length + 1);
+        people[array.length] = newPerson; // Also works: people[people.length - 1]
+        return people;
+    }
+
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
-        //this only points the address in memory
-        System.out.println(numbers);
+        //below only points the address in memory
+        System.out.println("\nAddress in memory: " + numbers);
         //this one prints out the elements in the array
-        System.out.println(Arrays.toString(numbers));
-
+        System.out.println("\nArray in string format: " + Arrays.toString(numbers) + "\n");
+        //Create array of people then we create persons
+        // and fill the array one by one using index
         Person[] people = new Person[3];
         people[0] = new Person("FirstPerson");
         people[1] = new Person("SecondPerson");
@@ -16,8 +24,12 @@ public class ArraysExercises {
         for(Person person : people){
             System.out.println(person.getName());
         }
+
+        System.out.println("\n************************************\nAdding Sophia to people array using addPerson method...\n");
+        Person sophia = new Person("Sophia");
+        people = addPerson(people, sophia);
+        for (Person person : people) {
+            System.out.println(person.getName());
+        }
     }
-//    public static addPerson{
-//
-//    }
 }
